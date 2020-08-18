6afea6a2f33b03c978992e3468b64466e4e526dc
fipooo <fma11541@gmail.com>
2020-08-18 10:38:20 +0200

    fipooo

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {
 "images/air2.png": "ee5608ef24e39e48f8a8851b09b61a60",
 "images/howwell1.png": "ac378a4643809c42e52d90f1f7a2ab62",
 "images/marsz2.png": "45c52e0b2eb63851ef6778c64196a9b2",
-"main.dart.js": "234794ce13de0b8cb53b16ef54219c50",
+"main.dart.js": "1854e771fa981459b10e18ceeea958e0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
